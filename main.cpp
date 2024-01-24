68b45a77ec49e8a2b9a9b5fe1e9b89b6631879b1
ANTHONY JOEL <157556538+ANTHONy-YGIA@users.noreply.github.com>
2024-01-23 23:45:50 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"======================================================"<<endl;
+    cout<<"NOMBRE: ANTHONY JOEL LARICO VELASQUEZ "<<endl;
+    cout<<"CODIGO: 236561"<<endl;
+    cout<<"======================================================"<<endl;
+    cout<<"PARA HALLAR EL AREA DEL CIRCULO"<<endl;
+    //Un programa para calcular el area de un circulo
+    //declarar variables
+    int radio, circulo ;
+    cout<<"Ingrese el radio:";
+    cin>>radio;
+    //operacion
+    circulo=3.14*radio*radio;
+    cout<<"El area del circulo es :"<<circulo<<endl;
+
+    return 0;
+}
