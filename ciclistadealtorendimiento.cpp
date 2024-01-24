ed7682851262a3240c3f2949deb86b8355016ce2
ANTHONY JOEL <157556538+ANTHONy-YGIA@users.noreply.github.com>
2024-01-24 01:26:13 -0500

    Create ciclistadealtorendimiento.cpp

---
+++ ciclistadealtorendimiento.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int r0, r1;
+	float r2 = 0;
+
+
+	cin >> r0;
+	cin >> r1;
+
+
+	r2 = (r0 + r1);
+
+
+  cout << r2 / 2;
+
+
+	return 0;
+}
