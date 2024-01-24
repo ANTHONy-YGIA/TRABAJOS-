a6358872e8444f656b51030bd026e0d26d5a6196
ANTHONY JOEL <157556538+ANTHONy-YGIA@users.noreply.github.com>
2024-01-24 01:28:55 -0500

    Create Dibujandouncorazon.cpp

---
+++ Dibujandouncorazon.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char v = 'c';
+	cout <<	   FFFFFF      FFFFF
+			  FFFFFFFF    FFFFFFF
+			 FFFFFFFFFF  FFFFFFFFF
+			FFFFFFFFFFFFFFFFFFFFFFF
+			FFFFFFFFFFFFFFFFFFFFFFF
+			 FFFFFFFFFFFFFFFFFFFFF
+			  FFFFFFFFFFFFFFFFFFF
+			   FFFFFFFFFFFFFFFFF
+			    FFFFFFFFFFFFFFF
+			     FFFFFFFFFFFFF
+			      FFFFFFFFFFF
+			       FFFFFFFFF
+			        FFFFFFF
+			         FFFFF
+			          FFF
+			           F
+
+
+
+	return 0
+}
